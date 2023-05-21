901a2d3527cf66a768ee6abf9dbe0d7870872a4d
Tom <45665177+TomTom2019@users.noreply.github.com>
2023-05-21 14:08:26 +0200

    reducer connecter
    reducer connecter

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
+import moviesReducer from './movies'
 
+// keyword is movies
 export const store = configureStore({
     reducer:{
+        movies: moviesReducer,
         
     }
 })
---
+++ src/store/movies.js
@@ -0,0 +1,14 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+// CRETED REDUCER  keyword is movies
+export const moviesSlice = createSlice({
+	name:'movies',
+	initialState:{},
+     reducers:{
+
+     }
+
+})
+
+
+export default moviesSlice.reducer
