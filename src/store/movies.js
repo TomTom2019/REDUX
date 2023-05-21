c6dc73c58f5fe071cf7fdc1ab88bd90ff8d5d6ac
Tom <45665177+TomTom2019@users.noreply.github.com>
2023-05-21 15:06:42 +0200

    add date store done
    add date store done

--- src/store/movies.js
+++ src/store/movies.js
@@ -1,27 +1,19 @@
-/*createSlice do all the action and type that we did before*/
-import {createSlice} from '@reduxjs/toolkit'
+import { createSlice } from "@reduxjs/toolkit";
 
-/*name must be reference here movies*/
+// initialState create list data
 export const moviesSlice = createSlice({
 	name:'movies',
 	initialState:{
 		list:[
-            {id:1,title:'Rambo'},
-            {id:2,title:'karate kid'},
-
+         {id:1,title:'Rambo'},
+         {id:2,title:'Superman'}
 			]
 	},
-	/*reducer and action are in the same spot  addMovie is the on to export*/
-	reducers:{
-     addMovie:(state,action)=>{
-       //const newMovie = {id:3,title:'Scarface'} method with paylod
-       state.list = [...state.list,action.payload]
+     reducers:{
+
      }
-	}
+
 })
 
 
-/* 1 the way to export action*/
-
-export const {addMovie} = moviesSlice.actions
 export default moviesSlice.reducer
