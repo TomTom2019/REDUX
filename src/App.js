b1ad7e15e731890c04ce6e75d717b67a987d4dd3
Tom <45665177+TomTom2019@users.noreply.github.com>
2023-05-26 14:49:26 +0200

    update
    update

--- src/App.js
+++ src/App.js
@@ -1,69 +1,35 @@
-/*HOOK REDUX you can get data or store data*/
-/* 2 the way to export dispatch action*/
-import { useEffect } from 'react'
-import {useSelector, useDispatch} from 'react-redux'
-import { addMovie } from './store/movies'
-import {setType, fechUsers/*getUsers*/} from './store/users'
+/*   */
+import { useSelector, useDispatch } from "react-redux";
+import { addMovie } from "./store/movies";
+import { setType,fetchUsers } from "./store/users";
 
-
-
-
+/*   */
 const App = () => {
+  const movies = useSelector((state) => state.movies.list);
+  const users = useSelector((state) => state.users);
+  const dispatch = useDispatch();
 
-const movies = useSelector((state)=> state.movies.list)
-const users = useSelector((state)=>state.users)
-const dispatch = useDispatch()
-
-/*you can use  same as ligne 54 here unwrap use then and catch */
- useEffect(()=>{
-dispatch(fechUsers())
-.unwrap()
-.then((response)=>{
-
- console.log(response)
-})
-.catch(error =>{
-  console.log(error)
-})
- },[])
-
-
-
-  return(
+  console.log(movies);
+/*   */
+  return (
     <>
       <h2>Movies</h2>
       <ul>
-        { movies ?
-         movies.map(movie => (
-            <li key={movie.id}>{movie.title} </li>
-          ))
-        :null
-
-        }
+        {movies
+          ? movies.map((movie) => <li key={movie.id}>{movie.title} </li>)
+          : null}
       </ul>
-      <hr/>
-     {/*  the way to export dispatch action*/}
-      <button onClick={()=>dispatch(addMovie({id:3,title:'Scarface'}))}>Add movies</button>
-      <hr/>
-      <h3>User type:{users.type} </h3>
-         {/*  the way to export dispatch action*/}
-      <button onClick={()=>dispatch(setType('Admin'))} >
-        Set type
+      <hr />
+      <button onClick={() => dispatch(addMovie({ id: 3, title: "batman" }))}>
+        Add movie
       </button>
-     
-
-         <div>{users.loading ? "loading" : null}</div>
-      <ul>{users ? users.users.map((user) =>
-        <li key={user.id}>{user.name}</li>) 
-      : null}</ul>
-      
-
-     
-     {/*54*/}
-      {/*<button onClick={() => dispatch(fechUsers())}>Get users</button>*/}
-      
+      <hr />
+      <h3>user type:{users.type} </h3>
+      <button onClick={() => dispatch(setType("Admin"))}>setType</button>
+      <hr/>
+      <button onClick={()=> dispatch(fetchUsers())}>get user</button>
     </>
-  )
-}
+  );
+};
 
 export default App;
