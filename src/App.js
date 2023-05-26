aed2d233fc042df17a7d11c2d23db6692f89c8f9
Tom <45665177+TomTom2019@users.noreply.github.com>
2023-05-26 14:28:53 +0200

    update done
    update done

--- src/App.js
+++ src/App.js
@@ -1,37 +1,35 @@
 //GET DATA // usedispath = action
-import {useSelector,useDispatch} from 'react-redux'
-import {addMovie} from './store/movies'
-import {setType} from './store/users'
+import { useSelector, useDispatch } from "react-redux";
+import { addMovie } from "./store/movies";
+import { setType,fetchUsers } from "./store/users";
 
 // movies is key from reducer
 const App = () => {
- const movies = useSelector((state)=> state.movies.list)
- const users = useSelector((state)=>state.users)
- const dispatch = useDispatch()
+  const movies = useSelector((state) => state.movies.list);
+  const users = useSelector((state) => state.users);
+  const dispatch = useDispatch();
 
-console.log(movies)
-// show data with loop with 
-  return(
+  console.log(movies);
+  // show data with loop with
+  return (
     <>
-    <h2>Movies</h2>
+      <h2>Movies</h2>
       <ul>
-        
-        { movies?
-         movies.map(movie =>(
-           <li key={movie.id}>{movie.title} </li>
-          ))
-
-        :null}
+        {movies
+          ? movies.map((movie) => <li key={movie.id}>{movie.title} </li>)
+          : null}
       </ul>
-      <hr/>
-      <button onClick={()=>dispatch(addMovie({id:3,title:'batman'}))}>Add movie</button>
-      <hr/>
-      <h3>user type:{users.type} </h3>
-      <button onClick={()=>dispatch(setType('Admin'))}>
-        setType
+      <hr />
+      <button onClick={() => dispatch(addMovie({ id: 3, title: "batman" }))}>
+        Add movie
       </button>
+      <hr />
+      <h3>user type:{users.type} </h3>
+      <button onClick={() => dispatch(setType("Admin"))}>setType</button>
+      <hr/>
+      <button onClick={()=> dispatch(fetchUsers())}>get user</button>
     </>
-  )
-}
+  );
+};
 
 export default App;
--- src/store/users.js
+++ src/store/users.js
@@ -1,17 +1,46 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice,createAsyncThunk } from "@reduxjs/toolkit";
+import axios from 'axios'
 
+// THUNKS WILL HANDLE RESQUEST
+// fetchUsers is key
+// this code work with extraReducers
+export const fetchUsers = createAsyncThunk(
+    "users/fetchUsers",
+    async (thunkAPI) => {
+        const res = await axios
+            .get("https://jsonplaceholder.typicode.com/users")
+            .then((response) => response.data);
+        return res;
+    }
+);
 
 export const userSlice = createSlice({
-	name:'users',
-	initialState:{
-		type:'Guest',
-		user:[]
-	},
-	reducers:{
-		setType:(state,action)=>{
-			state.type = action.payload || 'Guest'		}
-	}
-})
+    name: "users",
+    initialState: {
+        type: "Guest",
+        user: [],
+    },
+    reducers: {
+        setType: (state, action) => {
+            state.type = action.payload || "Guest";
+        }
+    },
 
-export const {setType} = userSlice.actions
-export default userSlice.reducer
+    /*YOU CAN DO ONLY FULFILLED IF YOU WANT*/
+    extraReducers:(builder)=>{
+        builder
+        .addCase(fetchUsers.pending,(state)=>{
+            console.log('pending')
+        })
+        .addCase(fetchUsers.fulfilled,(state,action)=>{
+            console.log('fulfilled')
+            state.user = action.payload
+        })
+           .addCase(fetchUsers.rejected,(state)=>{
+            console.log('rejected')
+        })
+    }
+});
+
+export const { setType } = userSlice.actions;
+export default userSlice.reducer;
