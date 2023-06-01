89e750ce56b5ab7d17f1f7e8e2fab00953431d24
Tom <45665177+TomTom2019@users.noreply.github.com>
2023-06-01 13:38:59 +0200

    rejectWithValue and fulfillWithValue show error*
    rejectWithValue and fulfillWithValue show error*

--- src/store/users.js
+++ src/store/users.js
@@ -1,84 +1,54 @@
-import {createSlice,createAsyncThunk} from '@reduxjs/toolkit'
-import axios from 'axios'
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
 
-/*createAsyncThunk is woking outside
-users is the key name here. fechUsers is the name reducer
-*/
-//8
-export const fechUsers = createAsyncThunk(
- 'users/fechUsers',
-     /* obj is first argument thunkAPI is second argument
+  
+export const fetchUsers = createAsyncThunk(
+    'users/fetchUsers',
+    async(obj,{ rejectWithValue, })=>{
+        // console.log(thunkAPI.getState())
+        // thunkAPI.dispatch(testAsyncDispatch())
+        try{
+            const res = await axios.get(`https://jsonplaceholder.typicode.com/users/111`)
+            //return res.data
 
-     */
-  async(obj,thunkAPI,)=>{
-  /*	you get everything from index store*/
-  /*	console.log(thunkAPI.getState())
-  	thunkAPI.dispatch(testAsyncDispatch())*/
-  	try{
-  		  const res = await axios.get(`https://jsonplaceholder.typicode.com/users/`)
-       /*.then(response => response.data);*/
+            //return fulfillWithValue('something else')
+        } catch(err){
+            return rejectWithValue('Oop, try again later')
+        }
+    }
+)
 
-  return res.data
- }catch(err){
-     return err
- }
-
-	}
-
-  	)
-
-/*be careful with correct spelling here i wrote user instead of users*/
-export const userSlice = createSlice({
-	name:'users',
-	initialState:{
-		type:'Guest',
-		users:[],
-		loading:false
-		/*test:false*/
-	},
-	reducers:{
-			/*reducer and action are in the same spot  setType is the on to export*/
-		setType:(state,action)=>{
-			state.type = action.payload || 'Guest'
-		},
-		/*this one trigger line 8*/
-		/*testAsyncDispatch:(state)=>{
-          state.test = true
-		}*/
-	/*	use axios to database - take time to be resolved
-      this code is syncro . with createAsyncThunk this code dosn't work
-	*/
-		/*getUsers:(state)=>{
-			axios.get('https://jsonplaceholder.typicode.com/users')
-			.then((response)=>{
-                 console.log(response.data)
-			})
-			.catch((err)=>{
-              console.log(err)
-			})
-		}*/
-	},
-	/*extraReducers is asynk thunk*/
-	extraReducers:(builder)=>{
-		builder.addCase(fechUsers.pending,(state)=>{
-			state.loading = true
-		})
-		/* fulfilled when everything is result + call back function
-           you can code only fulfilled no need pending and rejected
-		*/
-		.addCase(fechUsers.fulfilled,(state,action)=>{
-             state.loading = false
-            /*console.log(action.payload)*/
-            state.users = action.payload
-		})
-		.addCase(fechUsers.rejected,(state)=>{
-			console.log('rejected')
-		})
-	}
-
-
+export const usersSlice = createSlice({
+    name:'users',
+    initialState:{
+        type:'Guest',
+        users:[],
+        loading:false
+        //test:false
+    },
+    reducers:{
+        setType:(state,action)=>{
+            state.type = action.payload || 'Guest'
+        },
+        // testAsyncDispatch:(state)=>{
+        //     state.test = true
+        // }
+    },
+    extraReducers:(builder)=>{
+        builder
+        .addCase(fetchUsers.pending,(state)=>{
+           state.loading = true;
+        })
+        .addCase(fetchUsers.fulfilled,(state,action)=>{
+            state.loading = false;
+            state.users = action.payload;
+        })
+        .addCase(fetchUsers.rejected,(state,action)=>{
+            console.log(action.payload)
+        })
+    }
 })
 
-export const {setType,testAsyncDispatch } = userSlice.actions
 
-export default userSlice.reducer
+export const {setType,testAsyncDispatch} = usersSlice.actions;
+export default usersSlice.reducer;
