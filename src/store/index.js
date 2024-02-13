33d2850cc49f813d6fe62f9210912f67d10ccce6
Tom <45665177+TomTom2019@users.noreply.github.com>
2024-02-13 12:00:23 +0100

    update

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,5 @@
-
-import {configureStore} from '@reduxjs/toolkit'
-import moviesReducer from './movies'
-import usersReducer from './users'
-
-/*reducer do the end logic
-action fech information and go to reducer*/
+import { configureStore } from "@reduxjs/toolkit";
 
 export const store = configureStore({
-    reducer:{
-        movies:moviesReducer,
-        users: usersReducer
-    }
+    reducer:{}
 })
